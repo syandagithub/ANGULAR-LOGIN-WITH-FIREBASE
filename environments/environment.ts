3f190a866fd4828f9dd3d96001485e9a18fc21e3
syandagithub <112751864+syandagithub@users.noreply.github.com>
2023-03-22 14:29:29 +0200

    Add files via upload

---
+++ environments/environment.ts
@@ -0,0 +1,48 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  
+  firebaseConfig : {
+    apiKey: "AIzaSyCc8oHAzS18MtWq9v1TFy40BELJwL9v-CE",
+    authDomain: "siyeshee-2f01a.firebaseapp.com",
+    projectId: "siyeshee-2f01a",
+    storageBucket: "siyeshee-2f01a.appspot.com",
+    messagingSenderId: "147132007180",
+    appId: "1:147132007180:web:a3bb52aa413d51b7773f29",
+    measurementId: "G-8C3JRM5Q3M"
+
+  }
+};
+
+/*
+ 
+
+
+apiKey: "AIzaSyBsOueJogbo7b_4jfDEr-ogNpm4wikFal8",
+authDomain: "siyeshe-aad03.firebaseapp.com",
+projectId: "siyeshe-aad03",
+storageBucket: "siyeshe-aad03.appspot.com",
+messagingSenderId: "11683528848",
+appId: "1:11683528848:web:b7d0d4ed3128843cdad815"
+
+
+old
+
+apiKey: "AIzaSyBP508H4U6DklP15O08jlyx5NbJjT7mpXA",
+    authDomain: "hlehle-8a506.firebaseapp.com",
+    projectId: "hlehle-8a506",
+    storageBucket: "hlehle-8a506.appspot.com",
+    messagingSenderId: "1008164724276",
+    appId: "1:1008164724276:web:c2b9f62f5e9c4dd762b461"
+*/
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
